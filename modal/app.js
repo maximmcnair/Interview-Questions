9dcb78d763ac6d9450739afa34189bf15d1009ae
byverdu <byverdu@gmail.com>
2014-12-22 03:28:25 +0000

    refactored jquery code

--- modal/app.js
+++ modal/app.js
@@ -9,28 +9,28 @@ $(document).ready(function() {
       modal: true,
       width: '600px',
       appendTo: '.wrapper',
-      hide: { effect: 'fold', duration: 1000 },
+      hide: { effect: 'explode', duration: 1000 },
       close: function(){
       	$('body').css('background', 'RGB(255, 255, 255)');
+      },
+      open: function(){
+      	$('body').css('background', 'RGB(40, 42, 54)');
       }
     });
 
     // Showing modal window
     $('.popUp').on('click', function() {
-    	$('body').css('background', 'RGB(40, 42, 54)');
     	$('.modal').dialog('open');
 
     	// Closing window 
     	$('.modal').bind('click', function() {
     		$(this).dialog('close');
-    		$('body').css('background', 'RGB(255, 255, 255)');
     	});
     });
 
     // Custom closing the modal
 
     $('.closeModal').on('click', function() {
-    	
     	$('.modal').dialog('close');
     });
 });
